8c11a31ccbe507c824b8a2f5506307d5ad9390e6
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2022-09-28 15:47:43 +0300

    Add @ExperimentalTime to main function

--- src/main/kotlin/fi/hsl/transitdata/main.kt
+++ src/main/kotlin/fi/hsl/transitdata/main.kt
@@ -1,7 +1,9 @@
 package fi.hsl.transitdata
 
 import xyz.malkki.microservicetest.testexecution.MicroserviceTestExecutor
+import kotlin.time.ExperimentalTime
 
+@ExperimentalTime
 fun main() {
     MicroserviceTestExecutor.Companion.runMicroserviceTests()
 }
