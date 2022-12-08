48a2fdaa63208f868273dcfc0f8f840cf6b1fca5
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2022-12-08 15:38:40 +0200

    Add support for reading MQTT message payload from resource to SendMqttMessage step

--- src/main/kotlin/fi/hsl/transitdata/steps/SendMqttMessage.kt
+++ src/main/kotlin/fi/hsl/transitdata/steps/SendMqttMessage.kt
@@ -5,12 +5,16 @@ import org.testcontainers.containers.GenericContainer
 import xyz.malkki.microservicetest.testexecution.ParametrizedTestStepCode
 
 class SendMqttMessage : ParametrizedTestStepCode {
-    private fun sendMqttMessage(host: String, topic: String, payload: String) {
+    private fun sendMqttMessage(host: String, topic: String, payload: ByteArray) {
         val mqttClient = MqttClient(host, MqttClient.generateClientId())
         mqttClient.connect()
-        mqttClient.publish(topic, payload.toByteArray(Charsets.UTF_8), 1, false)
+        mqttClient.publish(topic, payload, 1, false)
         mqttClient.disconnect(1000)
         mqttClient.close(true)
+    }
+
+    private fun readPayloadFromResource(resourceName: String): ByteArray {
+        return SendMqttMessage::class.java.classLoader.getResourceAsStream(resourceName).readBytes()
     }
 
     override fun execute(
@@ -20,10 +24,13 @@ class SendMqttMessage : ParametrizedTestStepCode {
         getState: (key: String) -> Any?
     ) {
         val broker = containers[parameters["mqtt-broker-container"]!!]!!
+
+        val payload = parameters["payload"]?.toByteArray(Charsets.UTF_8) ?: readPayloadFromResource(parameters["payload-resource"]!!)
+
         sendMqttMessage(
             "tcp://${broker.host}:${broker.firstMappedPort}",
             parameters["topic"]!!,
-            parameters["payload"]!!
+            payload
         )
     }
 }
